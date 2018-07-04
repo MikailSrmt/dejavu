d12c5e6438113248ceab7622628d1643c0ace19c
MikailSeremet <mikail.seremet@gmail.com>
2018-07-03 21:24:24 -0700

    Simple DB Tables

--- ruler/models.py
+++ ruler/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
 # Create your models here.
+
+
+class Intervals(models.Model):
+    interval_name = models.CharField(max_length=200)
+    interval_date = models.DateTimeField('date published')
+
+
+class IntervalApply(models.Model):
+    interval_name = models.ForeignKey(Intervals, on_delete=models.CASCADE)
+    interval_description = models.CharField(max_length=200)
+    interval_counter = models.IntegerField(default=0)
