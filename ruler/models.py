6eb3e1959296994f62b2c8db8267c923c0a105d1
MikailSeremet <mikail.seremet@gmail.com>
2018-07-11 19:52:52 -0700

    Added Extensions

--- ruler/models.py
+++ ruler/models.py
@@ -1,4 +1,7 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 
@@ -6,8 +9,17 @@ class Intervals(models.Model):
     interval_name = models.CharField(max_length=200)
     interval_date = models.DateTimeField('date published')
 
+    def was_recorded_recently(self):
+        return self.interval_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return self.interval_name
+
 
 class IntervalApply(models.Model):
     interval_name = models.ForeignKey(Intervals, on_delete=models.CASCADE)
     interval_description = models.CharField(max_length=200)
     interval_counter = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.interval_name
